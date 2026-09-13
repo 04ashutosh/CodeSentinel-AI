2b2a37847ef7764b21e4473d818533ef7cfffa8c
Ashutosh Dash <ashutosh04004@gmail.com>
2026-09-13 11:35:55 +0530

    feat(ui): implement Dashboard layout and app routing

--- codesentinel-ui/src/app/components/dashboard/dashboard.ts
+++ codesentinel-ui/src/app/components/dashboard/dashboard.ts
@@ -1,8 +1,12 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ProjectUpload } from '../project-upload/project-upload';
+import { AiChat } from '../ai-chat/ai-chat';
 
 @Component({
   selector: 'app-dashboard',
-  imports: [],
+  standalone: true,
+  imports: [CommonModule,ProjectUpload,AiChat],
   templateUrl: './dashboard.html',
   styleUrl: './dashboard.scss',
 })
